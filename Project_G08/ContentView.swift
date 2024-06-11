ddc3bcbfbd4e1ad26781e730dcaa32cf71808384
Mandeep Neupane <30949446+np-mndp@users.noreply.github.com>
2024-06-10 22:40:45 -0400

    Initial Commit

---
+++ Project_G08/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project_G08
+//
+//  Created by Mandeep Neupane on 2024-06-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project_G08/Project_G08App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_G08App.swift
+//  Project_G08
+//
+//  Created by Mandeep Neupane on 2024-06-10.
+//
+
+import SwiftUI
+
+@main
+struct Project_G08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
